4588516b8847be3a6d235b261c80b7caa7ed12a2
TINGKAI1 <151415265+TINGKAI1@users.noreply.github.com>
2023-12-16 11:59:02 +0800

    Update pico9_2.py

--- pico_w/pico9_2.py
+++ pico_w/pico9_2.py
@@ -1,7 +1,13 @@
 from machine import Timer
 
 def callback1(t):
-    print(type(t))
+    print(1)
+    
+def callback2(t):
+    print(2)
     
 time1 = Timer()
 time1.init(freq=1,callback=callback1)
+
+time2 = Timer()
+time1.init(period=2000,callback=callback1)
