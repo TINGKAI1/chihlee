f2d52fe1f3a65da018d7687cac80f34c2e107bfe
TINGKAI1 <151415265+TINGKAI1@users.noreply.github.com>
2024-01-06 14:33:42 +0800

    Update pico11_2.py

--- pico_w/pico11_2.py
+++ pico_w/pico11_2.py
@@ -7,7 +7,7 @@ def connect():
     # enable station interface and connect to WiFi access point
     nic = network.WLAN(network.STA_IF)
     nic.active(True)
-    nic.connect('Robert_iPhone', '0926656000')
+    nic.connect('KAI', '88888888')
 
     max_wait = 10
 
