6a7b108124f528a6f078f956e58d23db0c563dcc
TINGKAI1 <151415265+TINGKAI1@users.noreply.github.com>
2024-01-06 10:26:47 +0800

    Update pico11_1.py

--- pico_w/pico11_1.py
+++ pico_w/pico11_1.py
@@ -5,15 +5,24 @@ nic.active(True)
 nic.connect('KAI','88888888')
 
 max_wait = 10
+
+#處理正在連線
 while max_wait > 0:
     max_wait -= 1
+    status = nic.status()
+    if status < 0 or status >=3:
+        break
+    print("等待連線")
     time.sleep(1)
 
-while not nic.isconnected() and nic.status() >= 0:
-    print("等待連線")
-    
-    
-print("有可能沒有wifi主機")
-print("已經連線成功")
 
-print(nic.ifconfig())
+#沒有wifi的處理
+if nic.status() != 3:
+    #連線失敗,重新開機
+    #wdt = WDT(timeout=2000)
+    #wdt.feed()
+    raise RuntimeError("連線失敗")
+    
+else:
+    print("連線成功")
+    print(nic.ifconfig())
