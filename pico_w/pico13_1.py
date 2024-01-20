3c4484de42ce56cf84cf0f34620d1c86ef7f89e4
TINGKAI1 <151415265+TINGKAI1@users.noreply.github.com>
2024-01-20 11:38:02 +0800

    Update pico13_1.py

--- pico_w/pico13_1.py
+++ pico_w/pico13_1.py
@@ -1,3 +1,9 @@
 from machine import Pin
+import time
+
 red_led = Pin(15,mode=Pin.OUT)
-red_led.value(1)
+btn = Pin(14,mode=Pin.PULL_DOWN)
+
+while True:
+    print(btn.value())
+    time.sleep_ms(500)
