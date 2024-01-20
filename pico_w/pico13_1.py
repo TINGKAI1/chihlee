3b0c969ef4d3c9776627c4044facb0a8f7a2ae46
TINGKAI1 <151415265+TINGKAI1@users.noreply.github.com>
2024-01-20 14:00:04 +0800

    Update pico13_1.py

--- pico_w/pico13_1.py
+++ pico_w/pico13_1.py
@@ -4,11 +4,20 @@ import time
 red_led = Pin(15,mode=Pin.OUT)
 btn = Pin(14,mode=Pin.PULL_DOWN)
 is_press = False
+led_status = False
+#switch button
+#解決彈跳
 
 while True:
     if btn.value():
-        is_press = True
+        time.sleep_ms(50)
+        if btn.value():
+            is_press = True
     elif is_press:
-        print('release')
-        is_press = False
-        
+        time.sleep_ms(50)
+        if btn.value() == False:
+            print('release')
+            led_status = not led_status        
+            red_led.value(led_status)        
+            is_press = False
+    
